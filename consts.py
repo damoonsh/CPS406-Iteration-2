cd951e21a9b52029c08e087b72f1d7a45e336b4e
Damoon Shah <damoon.shah3@gmail.com>
2021-04-02 07:28:54 +0430

    Minor change + documentation

--- consts.py
+++ consts.py
@@ -1,10 +1,18 @@
+"""
+    This file contains the constants used 
+    throughout the program
+"""
+# Colors
 BG_COLOR = (250, 245, 245)
 PLAYER_COLOR = (0, 255, 0)
 QIX_COLOR = (255, 0, 0)
 BORDER_COLOR = (0, 0, 0)
 
+# Constants used in the Game
 PLAYER_RADIUS = 10
 BORDER_THICKNESS = 2
 MARGIN = 20
 MAP_WIDTH = 420
 MAP_HEIGHT = 420
+QIX_DIM = 8
+SPARX_DIM = 6
