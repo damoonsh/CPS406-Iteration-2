d198bf67e2ede2918f79ce7479c80fec779047c6
Damoon Shah <damoon.shah3@gmail.com>
2021-04-02 07:28:34 +0430

    Qix is added

--- utils.py
+++ utils.py
@@ -23,9 +23,10 @@ class Player:
         Player Object that implements the logic of the
         player moving in the map.
     """
+
     def __init__(self):
         self.points = []
-        self.x = (consts.MAP_WIDTH - 2 * consts.MARGIN )// 2
+        self.x = (consts.MAP_WIDTH - consts.MARGIN) // 2
         self.y = 400
         self.claiming = False
         self.previous_direction = None
@@ -33,20 +34,26 @@ class Player:
     def get_coordinate(self):
         return (self.x, self.y)
 
-    def move(self, direction):
+    def move(self, direction: str):
+        """ Controls the movement of the player """
         if self.previous_direction != direction:
             self.previous_direction == direction
 
-        if self.claiming: self.points.append(Point(self.x, self.y, direction))
+        if self.claiming:
+            self.points.append(Point(self.x, self.y, direction))
 
-        if direction == 'right': self._move_right()
-        if direction == 'left': self._move_left()
-        if direction == 'up': self._move_up()
-        if direction == 'down': self._move_down()
+        if direction == 'right':
+            self._move_right()
+        if direction == 'left':
+            self._move_left()
+        if direction == 'up':
+            self._move_up()
+        if direction == 'down':
+            self._move_down()
 
     def _move_right(self):
         if self.x != consts.MAP_WIDTH - consts.MARGIN:
-            self.x += consts.PLAYER_RADIUS 
+            self.x += consts.PLAYER_RADIUS
 
     def _move_left(self):
         if self.x != consts.MARGIN:
@@ -55,16 +62,40 @@ class Player:
     def _move_up(self):
         if self.y != consts.MARGIN:
             self.y -= consts.PLAYER_RADIUS
-    
+
     def _move_down(self):
         if self.y != consts.MAP_HEIGHT - consts.MARGIN:
             self.y += consts.PLAYER_RADIUS
+
+
+class Enemy:
+    """
+        Manages the obstacles within the game 
+    """
+    class _Qix:
+        def __init__(self):
+            pass
+
+        def next_move(self):
+            pass
+
+    class _Sparx:
+        def __init__(self):
+            pass
+
+        def next_move(self):
+            pass
+
+    def __init__(self):
+        self.quixes = []
+        self.sparxes = []
 
 
 class Map:
     """
         Main map that renders everything within here
     """
+
     def __init__(self, height: int = consts.MAP_HEIGHT, width: int = consts.MAP_WIDTH):
         self.width = width
         self.height = height
@@ -93,6 +124,19 @@ class Map:
         self._draw_borders()
         self._draw_player()
         self._draw_clamied_areas()
+        self._draw_qix()
+
+    def _draw_sparx(self, x, y):
+        pygame.draw.polygon(self.gameDisplay,
+                            consts.QIX_COLOR,
+                            [(x, y), (x + consts.SPARX_DIM, y + consts.SPARX_DIM),
+                             (x, y + 2 * consts.SPARX_DIM), (x - consts.SPARX_DIM, y + consts.SPARX_DIM)])
+
+    def _draw_qix(self, x=(consts.MAP_WIDTH - consts.MARGIN) // 2, y=consts.MARGIN - consts.QIX_DIM):
+        pygame.draw.polygon(self.gameDisplay,
+                            consts.QIX_COLOR,
+                            [(x, y), (x + consts.QIX_DIM, y + consts.QIX_DIM),
+                             (x, y + 2 * consts.QIX_DIM), (x - consts.QIX_DIM, y + consts.QIX_DIM)])
 
     def _draw_clamied_areas(self):
         pass
@@ -116,21 +160,26 @@ class Map:
                    margin:self.height - margin] = color  # Right Vertical Line
 
 
+# Initialization
 PAUSE = False
 map = Map()
 map.render()
 
 while True:
     for event in pygame.event.get():
+
+        # Quitting the game
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
         if event.type == pygame.KEYDOWN:
 
+            # Starting to claim territory
             if event.key == pygame.K_SPACE:
                 map.player.claiming = not map.player.claiming
 
+            # Pausing
             if event.key == pygame.K_p:
                 PAUSE = True
 
@@ -140,13 +189,14 @@ while True:
 
             if event.key == pygame.K_LEFT:
                 map.player.move(direction="left")
-                
+
             if event.key == pygame.K_UP:
                 map.player.move(direction="up")
 
             if event.key == pygame.K_DOWN:
                 map.player.move(direction="down")
 
+    # If the game is not paused then render the graphics
     if not PAUSE:
         map.render()
 
